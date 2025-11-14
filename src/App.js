890628506f91e32ba1dc2dbdd12a9142b687125f
Gunasekar2001 <rgunasekar16082001@gmail.com>
2025-11-14 18:18:04 +0530

    fied

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import ScrollReveal from "scrollreveal";
 import "./App.css";
-import Guna from "./img/guna.png";
 import GunaAbout from "./img/guna-4.jpg";
 import work1 from "./img/work1.png";
 import work2 from "./img/work2.png";
@@ -239,7 +238,15 @@ const App = () => {
               </mask>
               <g mask="url(#mask0)">
                 <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z" />
-                <image class="home__blob-img" x="50" y="60" href={Guna} />
+                <image 
+                  className="home__blob-img" 
+                  x="0" 
+                  y="0" 
+                  width="479" 
+                  height="467" 
+                  href="/guna.jpg"
+                  preserveAspectRatio="xMidYMid slice"
+                />
               </g>
             </svg>
           </div>
